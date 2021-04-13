aba8dc23efe9a20af5df7a817b9bfdcc4ca4cd7f
Cícero Raupp Rolim <ironworld@gmail.com>
2021-04-13 20:08:04 -0300

    Aplicando um método para ser apresentado no sistema administrativo.

--- rh/admin.py
+++ rh/admin.py
@@ -4,12 +4,18 @@ from rh.models import Colaborador, Fornecedores
 
 class ColaboradorAdmin(admin.ModelAdmin):
 
-    list_display = ("nome", "emaildapessoa", "funcaodobrother", "cpf", "data_nascimento")
+    list_display = ("nome", "emaildapessoa", "funcaodobrother", "cpf", "data_do_nascimento")
+
+    def data_do_nascimento(self, obj):
+        if obj.data_nascimento:
+            return obj.data_nascimento.strftime("%d/%m/%Y")
+        return "-"
+    data_do_nascimento.short_description = 'Data Nasc.'
 
 admin.site.register(Colaborador, ColaboradorAdmin)
 
 class FornecedoresAdmin(admin.ModelAdmin):
-    
+
     list_display = ("titulo","oque", "datafornecedor")
 
 admin.site.register(Fornecedores, FornecedoresAdmin)
