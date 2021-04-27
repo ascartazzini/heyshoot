60a6cbf67587c1b46d1a40d0a8146c95edfb4df5
Cícero Raupp Rolim <ironworld@gmail.com>
2021-04-27 20:07:10 -0300

    Ajustando a listagem de todos os projetos.

--- rh/views.py
+++ rh/views.py
@@ -9,31 +9,31 @@ class ColaboradoresView(ListView):
     model = Colaborador
 
 class FornecedoresView(ListView):
-    
+
     context_object_name = "fornecedores"
     template_name = "fornecedores.html"
     model = Fornecedores
 
 class ClienteView(ListView):
-    
+
     context_object_name = "cliente"
     template_name = "clientes.html"
     model = Cliente
 
 class ProjetoView(ListView):
-    
-    context_object_name = "projeto"
+
+    context_object_name = "projetos"
     template_name = "projetos.html"
     model = Projeto
 
 class PropostaView(ListView):
-    
+
     context_object_name = "proposta"
     template_name = "propostas.html"
     model = Proposta
 
 class TipoProjetoView(ListView):
-    
+
     context_object_name = "TipoProjeto"
     template_name = "tipo_projeto.html"
     model = TipoProjeto
